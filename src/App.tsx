4e0e34171ae2cc5d6b34fd8d9596348328908e3a
raulcamunas <raulcamunas369@gmail.com>
2024-12-19 09:18:30 +0100

    Proyecto Finalizado

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import PatientForm from "./components/PatientForm";
+import PatientsList from "./components/PatientsList";
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
+
+export default function App() {
+  return (
+    <>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de pacientes {''}
+          <span className="text-indigo-700">Veterinaria</span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatientsList />
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  )
+}
